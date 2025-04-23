4ab40e1bb725a2f8dc997c037f519811faa520b4
devberatzengin <devberatzengin@gmail.com>
2025-04-23 16:51:28 +0300

    Word Adding Module Created And Included In The Project

--- WordAddForm.Designer.cs
+++ WordAddForm.Designer.cs
@@ -35,15 +35,23 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.engWordNameTb = new System.Windows.Forms.TextBox();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.turWordNameTb = new System.Windows.Forms.TextBox();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.wordPicLocationTb = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.sampleSentTb = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -82,11 +90,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 15.8F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(11, 15);
+            this.label1.Location = new System.Drawing.Point(11, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(266, 37);
+            this.label1.Size = new System.Drawing.Size(259, 37);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Engilish Word Name";
+            this.label1.Text = "English Word Name";
             // 
             // label2
             // 
@@ -102,7 +110,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 15.8F, System.Drawing.FontStyle.Bold);
-            this.label4.Location = new System.Drawing.Point(11, 17);
+            this.label4.Location = new System.Drawing.Point(11, 19);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(308, 37);
             this.label4.TabIndex = 3;
@@ -112,67 +120,146 @@
             // 
             this.panel2.BackColor = System.Drawing.Color.MediumSeaGreen;
             this.panel2.Controls.Add(this.label1);
-            this.panel2.Location = new System.Drawing.Point(0, 98);
+            this.panel2.Location = new System.Drawing.Point(-2, 86);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(337, 68);
             this.panel2.TabIndex = 4;
             // 
-            // textBox1
+            // engWordNameTb
             // 
-            this.textBox1.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 14F, System.Drawing.FontStyle.Bold);
-            this.textBox1.Location = new System.Drawing.Point(382, 116);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(301, 39);
-            this.textBox1.TabIndex = 4;
+            this.engWordNameTb.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 14F, System.Drawing.FontStyle.Bold);
+            this.engWordNameTb.Location = new System.Drawing.Point(380, 104);
+            this.engWordNameTb.Name = "engWordNameTb";
+            this.engWordNameTb.Size = new System.Drawing.Size(339, 39);
+            this.engWordNameTb.TabIndex = 4;
             // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.MediumSeaGreen;
             this.panel3.Controls.Add(this.label2);
-            this.panel3.Location = new System.Drawing.Point(0, 220);
+            this.panel3.Location = new System.Drawing.Point(-2, 177);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(337, 68);
             this.panel3.TabIndex = 5;
             // 
-            // textBox2
+            // turWordNameTb
             // 
-            this.textBox2.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 14F, System.Drawing.FontStyle.Bold);
-            this.textBox2.Location = new System.Drawing.Point(382, 239);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(301, 39);
-            this.textBox2.TabIndex = 4;
+            this.turWordNameTb.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 14F, System.Drawing.FontStyle.Bold);
+            this.turWordNameTb.Location = new System.Drawing.Point(380, 196);
+            this.turWordNameTb.Name = "turWordNameTb";
+            this.turWordNameTb.Size = new System.Drawing.Size(339, 39);
+            this.turWordNameTb.TabIndex = 4;
             // 
             // panel4
             // 
             this.panel4.BackColor = System.Drawing.Color.MediumSeaGreen;
             this.panel4.Controls.Add(this.label4);
-            this.panel4.Location = new System.Drawing.Point(0, 349);
+            this.panel4.Location = new System.Drawing.Point(-2, 270);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(337, 68);
             this.panel4.TabIndex = 6;
             // 
-            // textBox3
+            // wordPicLocationTb
             // 
-            this.textBox3.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 14F, System.Drawing.FontStyle.Bold);
-            this.textBox3.Location = new System.Drawing.Point(382, 369);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(301, 39);
-            this.textBox3.TabIndex = 4;
+            this.wordPicLocationTb.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 14F, System.Drawing.FontStyle.Bold);
+            this.wordPicLocationTb.Location = new System.Drawing.Point(380, 290);
+            this.wordPicLocationTb.Name = "wordPicLocationTb";
+            this.wordPicLocationTb.Size = new System.Drawing.Size(339, 39);
+            this.wordPicLocationTb.TabIndex = 4;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.saveButton.Location = new System.Drawing.Point(561, 465);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(227, 47);
+            this.saveButton.TabIndex = 7;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.panel5.Controls.Add(this.label5);
+            this.panel5.Location = new System.Drawing.Point(-2, 366);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(337, 68);
+            this.panel5.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 15.8F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(11, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(313, 37);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Word\'s sample sentence";
+            // 
+            // sampleSentTb
+            // 
+            this.sampleSentTb.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 14F, System.Drawing.FontStyle.Bold);
+            this.sampleSentTb.Location = new System.Drawing.Point(380, 386);
+            this.sampleSentTb.Name = "sampleSentTb";
+            this.sampleSentTb.Size = new System.Drawing.Size(339, 39);
+            this.sampleSentTb.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(55, 482);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(103, 20);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(198, 482);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(103, 20);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(333, 482);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(103, 20);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
             // 
             // WordAddForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(820, 450);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.textBox2);
+            this.ClientSize = new System.Drawing.Size(820, 539);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.sampleSentTb);
+            this.Controls.Add(this.wordPicLocationTb);
+            this.Controls.Add(this.engWordNameTb);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.turWordNameTb);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "WordAddForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "WordAddForm";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.WordAddForm_FormClosing);
             this.panel1.ResumeLayout(false);
@@ -183,6 +270,8 @@
             this.panel3.PerformLayout();
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -198,9 +287,16 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox engWordNameTb;
+        private System.Windows.Forms.TextBox turWordNameTb;
         private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox wordPicLocationTb;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox sampleSentTb;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
     }
 }
--- WordAddForm.cs
+++ WordAddForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,6 +18,10 @@ namespace Word_Memorizing_Game
             InitializeComponent();
         }
 
+
+        
+
+
         private void mainMenuButton_Click(object sender, EventArgs e)
         {
             MainMenuForm mainMenuForm = new MainMenuForm();
@@ -30,6 +35,83 @@ namespace Word_Memorizing_Game
             Application.Exit(); 
         }
 
-        
+
+
+        private readonly string connectionString = "Data Source=BERATZ\\SQLEXPRESS;Initial Catalog=GameDb;Integrated Security=True";
+
+
+
+
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(engWordNameTb.Text) || string.IsNullOrWhiteSpace(turWordNameTb.Text) || string.IsNullOrWhiteSpace(wordPicLocationTb.Text) || string.IsNullOrWhiteSpace(sampleSentTb.Text))
+            {
+                MessageBox.Show("Lütfen hicbir alanı bos birakmayiniz!");
+                return;
+            }
+
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand checkWordCommand = new SqlCommand("SELECT COUNT(*) FROM tblWords WHERE EngWordName = @wordName", connection);
+                checkWordCommand.Parameters.AddWithValue("@wordName", engWordNameTb.Text);
+
+                int wordExists = (int)checkWordCommand.ExecuteScalar();
+
+                if (wordExists > 0)
+                {
+                    MessageBox.Show("Bu kelime zaten mevcut!");
+                    return;
+                }
+
+                SqlCommand getMaxIdCommand = new SqlCommand("SELECT ISNULL(MAX(WordID), 0) FROM tblWords", connection);
+                int nextId = (int)getMaxIdCommand.ExecuteScalar() + 1;
+                
+                
+                SqlCommand insertCommand = new SqlCommand("INSERT INTO tblWords (WordID, EngWordName, TurWordName, Picture) VALUES (@id, @engWordName, @turWordName, @wordPicLocation)", connection);
+                insertCommand.Parameters.AddWithValue("@id", nextId);
+                insertCommand.Parameters.AddWithValue("@engWordName", engWordNameTb.Text);
+                insertCommand.Parameters.AddWithValue("@turWordName", turWordNameTb.Text);
+                insertCommand.Parameters.AddWithValue("@wordPicLocation", wordPicLocationTb.Text);
+
+
+                SqlCommand insertSampleCommand = new SqlCommand("INSERT INTO tblWordSamples (Samples, WordID) VALUES (@sampleSentence, @wordId)", connection);
+                insertSampleCommand.Parameters.AddWithValue("@sampleSentence", sampleSentTb.Text);
+                insertSampleCommand.Parameters.AddWithValue("@wordId", nextId);
+
+
+
+                int result = insertCommand.ExecuteNonQuery();
+                int sampleResult = insertSampleCommand.ExecuteNonQuery();
+
+
+                if (result > 0)
+                {
+                    MessageBox.Show("Kelime başarıyla eklendi!");
+                }
+                else
+                {
+                    MessageBox.Show("Kelime eklenirken bir hata oluştu.");
+                }
+                
+                if (sampleResult > 0)
+                {
+                    MessageBox.Show("Örnek cümle başarıyla eklendi!");
+                }
+                else
+                {
+                    MessageBox.Show("Örnek cümle eklenirken bir hata oluştu.");
+                }
+
+
+
+
+
+            }
+            
+        }
     }
 }
+
